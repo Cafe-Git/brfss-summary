1b11b9d8e36e005546b02c35bd67a3385d7206cb
asenetcky <asenetcky@gmail.com>
2025-05-10 07:56:35 -0400

    chore: add todo on fwf implementation

--- R/read-acii.R
+++ R/read-acii.R
@@ -6,7 +6,11 @@ if (!fs::file_exists(ASCII_FILE_PATH)) cli::cli_alert_danger("No ASCII Data.")
 # for now just do a cold read for testing
 
 brffs <-
-  readr::read_fwf(ASCII_FILE_PATH)
+  readr::read_fwf(
+    file = ASCII_FILE_PATH #,
+    # TODO: use fwf_cols to provide names and start/end positions
+    #fwf_cols()
+  )
 
 # TODO: implement minimally viable wrangle
 # TODO:  save to parquet file in data_prooduct/
